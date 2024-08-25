0d6a9389842b88f4266c40db5a541ca2e8060caa
richardbilan <157667872+richardbilan@users.noreply.github.com>
2024-08-25 15:37:40 +0800

    Add files via upload
    My first web project

---
+++ FINAL PROJECT/QuizQuestions.js
@@ -0,0 +1,168 @@
+let questions = [
+    {
+        numb: 1,
+        question: " Who is known as the national hero of the Philippines?",
+        answer: "A. José Rizal",
+        options: [
+            "A. José Rizal",
+            "B. Andres Bonifacio",
+            "C. Emilio Aguinaldo",
+            "D. Emilio Aguinaldo"
+
+        ]
+    },
+    {
+        numb: 2,
+        question: "Who is known as the Father of the Philippine Revolution?",
+        answer: "B. Andres Bonifacio",
+        options: [
+            "A. José Rizal",
+            "B. Andres Bonifacio",
+            "C. Marcelo H. del Pilar",
+            "D. Antonio Luna"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which national hero is often referred to as the Brains of Revolution",
+        answer: "B. Apolinario Mabini",
+        options: [
+            "A. Emilio Aguinaldo",
+            "B. Apolinario Mabini",
+            "C. Marcelo H. del Pilar",
+            "D. Antonio Luna"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Who was the first and the youngest president of the Philippines?",
+        answer: "A. Emilio Aguinaldo",
+        options: [
+            "A. Emilio Aguinaldo",
+            "B. Apolinario Mabini",
+            "C. Antonio Luna",  
+            "D. George Dewey"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Who was the first recorded hero in Philippine history?",
+        answer: "D. Lapu-lapu",
+        options: [
+            "A. José Rizal",
+            "B. Andres Bonifacio",
+            "C. Antonio Luna",
+            "D. Lapu-lapu"
+        ]
+    },
+    {
+        numb: 6,
+        question: "What is the national bird of the Philippines?",
+        answer: "B. Philippine Eagle",
+        options: [
+            "A. Maya",
+            "B. Philippine Eagle",
+            "C. Sarimanok",
+            "D. Tamaraw"
+        ]
+    },
+    {
+        numb: 7,
+        question: "The national flower of the Philippines is known by what name?",
+        answer: "A. Sampaguita",
+        options: [
+            "A. Sampaguita",
+            "B. Waling-waling",
+            "C. Rosal",
+            "D. Gumamela"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Which animal is considered the national symbol of the Philippines?",
+        answer: "A. Carabao",
+        options: [
+            "A. Carabao",
+            "B. Tarsier",
+            "C. Butanding (Whale Shark)",
+            "D. Dugong"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What is the national tree of the Philippines?",
+        answer: "A. Narra",
+        options: [
+            "A. Narra",
+            "B. Acacia",
+            "C. Mahogany",
+            "D. Molave"
+        ]
+    },
+    {
+        numb: 10,
+        question: " What is the national gem of the Philippines?",
+        answer: "D. Philippine Pearl",
+        options: [
+            "A. Diamond",
+            "B. Ruby",
+            "C. Emerald",
+            "D. Philippine Pearl"
+        ]
+    },
+    {
+        numb: 11,
+        question: "Which of the following is a UNESCO World Heritage Site and considered one of the new Seven Wonders of Nature in the Philippines?",
+        answer: "D. Underground River in Puerto Princesa",
+        options: [
+            "A. Banaue Rice Terraces",
+            "B. Chocolate Hills",
+            "C. Mayon Volcano",
+            "D. Underground River in Puerto Princesa"
+        ]
+    },
+    {
+        numb: 12,
+        question: "This historical site in Manila, also known as the Walled City, is a UNESCO World Heritage Site and was the seat of government during the Spanish colonial period.",
+        answer: "C. Intramuros",
+        options: [
+            "A. Fort Santiago",
+            "B. Rizal Park",
+            "C. Intramuros",
+            "D. Corregidor Island"
+        ]
+    },
+    {
+        numb: 13,
+        question: "What is the Banaue Rice Terraces?",
+        answer: "A. It is a UNESCO World Heritage Site.",
+        options: [
+            "A. It is a UNESCO World Heritage Site.",
+            "B. It is the largest rice plantation in the Philippines.",
+            "C. It is a mountain range in northern Luzon.",
+            "D. It is a popular beach resort."
+        ]
+    },
+    {
+        numb: 14,
+        question: "Where is the site of General Douglas MacArthur’s return to the Philippines?",
+        answer: "A. Leyte Landing Memorial Park",
+        options: [
+            "A. Leyte Landing Memorial Park",
+            "B. Rizal Park",
+            "C. Luneta Park",
+            "D. Manila Bay"
+        ]
+    },
+    {
+        numb: 15,
+        question: "What is the significance of the Chocolate Hills in Bohol?",
+        answer: "B. They are a unique geological formation and a popular tourist attraction.",
+        options: [
+            "A. They are the site of a major archaeological discovery.",
+            "B. They are a unique geological formation and a popular tourist attraction.",
+            "C. They are the site of a historic battle.",
+            "D. They are considered sacred by local indigenous communities."
+        ]
+    },
+];
